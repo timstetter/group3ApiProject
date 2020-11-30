112107afc101347686c424f61e351d118bfb16ef
timstetter <tstetter@gmail.com>
2020-11-30 14:47:10 -0500

    removed unused imports, fixed next page error

--- src/main/java/co/grandcircus/group3apiproject/EventController.java
+++ src/main/java/co/grandcircus/group3apiproject/EventController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import co.grandcircus.group3apiproject.model.Event;
-import co.grandcircus.group3apiproject.model.EventResponse;
 
 
 
--- src/main/java/co/grandcircus/group3apiproject/model/Event.java
+++ src/main/java/co/grandcircus/group3apiproject/model/Event.java
@@ -4,14 +4,10 @@ package co.grandcircus.group3apiproject.model;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name="event")
